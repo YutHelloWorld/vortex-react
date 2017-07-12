f75c2639fd7544591e3bfd20c184edbd91c49771
Sven <sven@139.com>
2017-07-13 02:28:44 +0800

    Change favicon

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import VortexImage from '../assets/vortex.png'
 import './HomeView.scss'
 
 export const HomeView = () => (
   <div>
     <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+    <img alt='This is a duck, because Redux!' className='duck' src={VortexImage} />
   </div>
 )
 
