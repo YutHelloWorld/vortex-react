19e1866dbd2ae7ef23cf8a5667e45c8f7e4551d7
YutHelloWorld <stevencode@163.com>
2017-07-14 10:31:33 +0800

    add comments

--- src/routes/Zen/index.js
+++ src/routes/Zen/index.js
@@ -4,6 +4,8 @@ export default (store) => ({
   path: 'zen',
   /*  Async getComponent is only invoked when route matches   */
   getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+        and embed an async module loader (jsonp) when bundling   */
     require.ensure([], (require) => {
       const Zen = require('./containers/ZenContainer').default
       const reducer = require('./modules/zen').default
