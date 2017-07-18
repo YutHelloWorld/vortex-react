8a81ff4d23121f1294296501c109c5e0fb2def66
Sven <sven@139.com>
2017-07-18 10:53:12 +0800

    use navLink with reac-router

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import { Link, IndexLink } from 'react-router'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container } from 'reactstrap'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
 import { connect } from 'react-redux'
@@ -10,26 +11,40 @@ export const PageLayout = ({ children, layout : { isOpen }, toggle }) => (
     <div>
       <Navbar color='faded' light toggleable>
         <NavbarToggler right onClick={toggle} />
-        <NavbarBrand href='/'>Vortex React</NavbarBrand>
+        <NavbarBrand to='/' tag={IndexLink}>Vortex React</NavbarBrand>
         <Collapse isOpen={isOpen} navbar>
           <Nav className='ml-auto' navbar>
             <NavItem>
-              <NavLink href='/components/'>Components</NavLink>
+              <NavLink to='/counter/' activeClassName='active' tag={Link}>Counter</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href='https://github.com/reactstrap/reactstrap'>Github</NavLink>
+              <NavLink to='/zen' activeClassName='active' tag={Link}>Zen</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/elapse' activeClassName='active' tag={Link}>Elapse</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/route/88' activeClassName='active' tag={Link}>Route</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/notFound' activeClassName='active' tag={Link}>404</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='https://github.com/YutHelloWorld/vortex-react'>Github</NavLink>
             </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
     </div>
-
+    <Container className='text-center' style={{ marginTop: '2rem' }}>
+      {children}
+    </Container>
   </div>
 )
 PageLayout.propTypes = {
-  children: PropTypes.node,
-  layout  : PropTypes.object.isRequired,
-  toggle  : PropTypes.func.isRequired
+  children : PropTypes.node,
+  layout   : PropTypes.object.isRequired,
+  toggle   : PropTypes.func.isRequired
 }
 
 const mapDispatchToProps = {
