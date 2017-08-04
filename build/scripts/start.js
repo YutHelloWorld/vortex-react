0aec67891f14264ab0b65aed776f2d3b100687fd
Keifer Gu <keifergu@gmail.com>
2017-08-04 14:01:33 +0800

    [feat]: 添加自动打开浏览器对应链接的功能

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,9 @@
 const logger = require('../lib/logger')
+const opn = require('opn')
 
 logger.info('Starting server...')
 require('../../server/main').listen(3000, () => {
   logger.success('==> 🌎  Server is running at http://localhost:3000')
 })
+
+opn('http://localhost:3000')
