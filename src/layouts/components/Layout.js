269f929b7f0457415b141db1f480e20d37ffd759
Sven <sven@139.com>
2017-09-22 19:29:24 +0800

    refactor: routes

--- src/layouts/components/Layout.js
+++ src/layouts/components/Layout.js
@@ -6,11 +6,10 @@ import Navbar from '../containers/NavbarContainer'
 import './Layout.scss'
 import asyncComponent from 'components/AsyncComponent'
 import PrivateRoute from 'components/PrivateRoute'
+import Login from 'routes/Login'
+import Home from 'routes/Home'
 
 // `import()` 异步加载模块，魔法注释写chunkName
-const AsyncHome = asyncComponent(() => import(
-  /* webpackChunkName: "home" */
-  'routes/Home'))
 const AsyncCounter = asyncComponent(() => import(
   /* webpackChunkName: "counter" */
   'routes/Counter'))
@@ -27,23 +26,19 @@ const AsyncPageNotFound = asyncComponent(() => import(
   /* webpackChunkName: "pageNotFound" */
   'routes/PageNotFound'))
 
-const AsyncLogin = asyncComponent(() => import(
-  /* webpackChunkName: "login" */
-  'routes/Login'))
-
 export default function Layout() {
   return (
     <div>
       <Navbar />
       <Container className="text-center page-layout__viewport">
         <Switch>
-          <PrivateRoute exact path="/" component={AsyncHome} />
+          <PrivateRoute exact path="/" component={Home} />
           <PrivateRoute path="/counter" component={AsyncCounter} />
           <PrivateRoute path="/zenPage" component={AsyncZen} />
           <PrivateRoute path="/elapse" component={AsyncElapse} />
           <PrivateRoute path="/route/:id" component={AsyncRoute} />
           <PrivateRoute path="/404" component={AsyncPageNotFound} />
-          <Route path="/login" component={AsyncLogin} />
+          <Route path="/login" component={Login} />
           <Redirect from="*" to="/404" />
         </Switch>
       </Container>
